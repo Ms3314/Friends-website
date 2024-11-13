6c99c8dd2bd55f46952c3bd681f6a478987f6e62
Samiuddin <mdsamiuddin2005@outlook.com>
2024-11-13 12:32:42 +0530

    shit

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
 }));
 // Create the Express app
-
+// shit 
 
 emailRegex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/i;
 
